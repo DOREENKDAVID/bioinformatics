b63c7cc230efdddffd957b64215a3181791543e2
DOREENKDAVID <doreenkinya153@gmail.com>
2024-03-01 23:44:13 +0300

    miniproject on files manipulation in bioinformatics

---
+++ Exercise/Data/Processed_data/total_count.sh
@@ -0,0 +1,9 @@
+#total  per base
+
+ 430760 A
+ 369132 C
+ 387672 G
+ 432900 T
+
+# total bases
+1620497
---
+++ Exercise/Scripts/extract_seq.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+# headers of the datasets
+grep ">" nrf1_seq.fa nrf1_seqtemp.fa > sequence_names.txt
+
+#lines with mRNA headers
+grep "mRNA" sequence_names.txt | wc -l
+
+#lines count without mRNA headers
+grep -v "mRNA" sequence_names.txt | wc -l
+
+#lines count without mRNA headers and sequences
+grep  -v "mRNA" nrf1_seq.fa nrf1_seqtemp.fa > DNA_sequenes.fa
+
+#lines with mRNA headers and sequences
+grep "mRNA" nrf1_seq.fa nrf1_seqtemp.fa > mRNA_sequences.fa
+
+#lines with mRNA headers
+grep "mRNA" sequence_names.txt > mRNA.fa
+
+#lines without mRNA headers
+grep  -v "mRNA" sequence_names.txt > DNA.fa
+
+#merge two files
+cat nrf1_seq.fa nrf1_seqtemp.fa > merged.fa
+
+
+#unique sequences 
+uniq merged.fa > uniq_seq.fa
+
+# Count the total number of nucleotides
+grep -v "^>" uniq_seq.fa | tr -d '\n' | wc -c
+
+# Count the frequency of each base
+grep -v "^>" uniq_seq.fa | grep -o . | sort | uniq -c
+
+
